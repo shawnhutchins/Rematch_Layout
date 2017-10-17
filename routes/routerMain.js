d1bad21f7dd3a4c127ee1c3687774668d0c86230
Shawn Hutchins <shawnhutchins1@gmail.com>
2017-10-17 01:09:22 -0400

    Finished partials and cleaned up about page

--- routes/routerMain.js
+++ routes/routerMain.js
@@ -7,7 +7,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/about', function( req, res, next) {
-  res.render('pages/about');
+  var gamerBios = [
+    { 
+      name: 'GenderLizard',
+      image: 'images/genderlizard_icon.png',
+      bio: 'The bio for genderlizard, it IS beautiful and nice. It also includes relevant information.',
+      twitch: 'https://www.twitch.tv/rematch_gaming',
+      youtube: 'https://www.youtube.com/channel/UCHyj0igLYfZbGcKRgF8Dhyg',
+      twitter: 'https://twitter.com/MaddHabitGaming',
+    },
+    { 
+      name: 'llatlasll',
+      image: 'images/llatlasll_icon.png',
+      bio: 'This is a bio full of relevant information, all about llatlasll and why that is his name.',
+      twitch: 'https://www.twitch.tv/rematch_gaming',
+      youtube: 'https://www.youtube.com/channel/UCHyj0igLYfZbGcKRgF8Dhyg',
+      twitter: 'https://twitter.com/MaddHabitGaming',
+    },
+  ];
+
+  res.render('pages/about', {
+    gamerBios : gamerBios,
+  });
 });
 
 router.get('/stream', function( req, res, next) {
