d099411f9aff6bd3504c94068a3af8535d3b56b1
Shawn Hutchins <shawnhutchins1@gmail.com>
2017-10-20 19:00:37 -0400

    Updated slideshow to changes every 5 seconds

--- public/javascripts/slideshow.js
+++ public/javascripts/slideshow.js
@@ -1,5 +1,24 @@
 var slideIndex = 1;
+var slideTimer; 
+var slideDelay = 5000;
+var mySlides = document.getElementsByClassName("mySlides");
 showDivs(slideIndex);
+
+function startSlideTimer() {
+  slideTimer = setTimeout(function(){
+    slideIndex++;
+    if ( slideIndex > mySlides.length ) {
+      slideIndex = 1;
+    }
+    showDivs(slideIndex);
+  }, slideDelay);
+  console.log("timer  set"); 
+}
+
+function stopSlideTimer() {
+  clearTimeout(slideTimer);
+  console.log("clearing the timer");
+}
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
@@ -10,17 +29,31 @@ function currentDiv(n) {
 }
 
 function showDivs(n) {
+  console.log(n + " n at the start");
+  console.log(slideIndex + " slideIndex at the start");
   var i;
-  var x = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1;}
-  if (n < 1) {slideIndex = x.length;}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  //if the value passed is larger then there are slides se back to the start
+  if (n > mySlides.length) {
+    slideIndex = 1;}
+  //if the valude passed is less than 1 set to the last slide
+  if (n < 1) {
+    slideIndex = mySlides.length;
   }
+  //make all the slides not visible
+  for (i = 0; i < mySlides.length; i++) {
+    mySlides[i].style.display = "none";
+  }
+  //make the selected slide visible
+  console.log(slideIndex + " slideIndex right before error");
+  mySlides[slideIndex-1].style.display = "block";
+
+  //removes the w3-white class with nothing making them empty
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
+    dots[i].className = dots[i].className.replace(" w3-white", ""); 
   }
-  x[slideIndex-1].style.display = "block";
+  //makes the dot that represents the current slide white
   dots[slideIndex-1].className += " w3-white";
+  stopSlideTimer();
+  startSlideTimer();
 }
