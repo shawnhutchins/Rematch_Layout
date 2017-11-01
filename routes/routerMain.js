283dbaec85c9b9301ac0380531a248ec04e52d90
Shawn Hutchins <shawnhutchins1@gmail.com>
2017-10-31 20:16:57 -0400

    Changed img links

--- routes/routerMain.js
+++ routes/routerMain.js
@@ -10,19 +10,27 @@ router.get('/about', function( req, res, next) {
   var gamerBios = [
     { 
       name: 'GenderLizard',
-      image: 'images/genderlizard_icon.png',
+      image: 'https://static-cdn.jtvnw.net/jtv_user_pictures/86440cc77f5c995e-profile_image-300x300.png',
       bio: 'The bio for genderlizard, it IS beautiful and nice. It also includes relevant information.',
-      twitch: 'https://www.twitch.tv/rematch_gaming',
+      twitch: 'https://www.twitch.tv/genderlizard',
+      youtube: 'https://www.youtube.com/channel/UCmP-HDKtj2zuUTKJYb70H_Q',
+      twitter: 'https://www.twitch.tv/genderlizard',
+    },
+    {
+      name: 'B00cup',
+      image: 'https://static-cdn.jtvnw.net/jtv_user_pictures/7f8f253f13811620-profile_image-300x300.jpeg',
+      bio: 'This bio for B00cup, is very accurate. Much attention was paid to all the details.',
+      twitch: 'https://www.twitch.tv/b00cup',
       youtube: 'https://www.youtube.com/channel/UCHyj0igLYfZbGcKRgF8Dhyg',
-      twitter: 'https://twitter.com/MaddHabitGaming',
+      twitter: 'https://twitter.com/ReMatch_B00cup'
     },
     { 
       name: 'llatlasll',
-      image: 'images/llatlasll_icon.png',
+      image: 'https://static-cdn.jtvnw.net/jtv_user_pictures/bbdbf217d0a6e49c-profile_image-300x300.png',
       bio: 'This is a bio full of relevant information, all about llatlasll and why that is his name.',
-      twitch: 'https://www.twitch.tv/rematch_gaming',
+      twitch: 'https://www.twitch.tv/llatlasll',
       youtube: 'https://www.youtube.com/channel/UCHyj0igLYfZbGcKRgF8Dhyg',
-      twitter: 'https://twitter.com/MaddHabitGaming',
+      twitter: 'https://twitter.com/ReMatch_Atlas',
     },
   ];
 
